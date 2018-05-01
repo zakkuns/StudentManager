3d4aeded55945c5cba9d4b7ae2c324b758fb4188
zakkuns <zkuns@mail.csuchico.edu>
2018-05-01 12:06:48 -0700

    Header file for class student

---
+++ student.h
@@ -0,0 +1,25 @@
+//Author: Zakary Kuns
+
+#include <iostream>
+#include <string>
+
+#ifndef STUDENT_H_
+#define STUDNET_H_
+
+class student
+{
+
+public:
+
+void setName(string, string);
+
+private:
+
+string lastname;
+string firstname;
+
+};
+
+
+
+#endif
